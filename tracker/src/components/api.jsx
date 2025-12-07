4b2cf143ecc6339d5a314aa071f40d544edd61e1
Mekha <mohanananmekha04@gmail.com>
2025-12-07 15:50:39 +0530

    api added

--- tracker/src/components/api.jsx
+++ tracker/src/components/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const apiUrl=import.meta.env.VITE_API_URL
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: apiUrl,
     withCredentials: true,
 });
 
