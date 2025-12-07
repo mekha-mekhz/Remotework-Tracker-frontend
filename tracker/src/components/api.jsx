0d3196e4b4839b1892ac72b3e2c7c7ac22e6a9fa
Mekha Mohanan <157354677+mekha-mekhz@users.noreply.github.com>
2025-12-07 23:30:26 +0530

    Update api.jsx

--- tracker/src/components/api.jsx
+++ tracker/src/components/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://remotework-tracker-backend.onrender.com/api",
+  baseURL: "https://remotework-tracker-backend.onrender.com",
   withCredentials: true, // only needed if cookies are used
 });
 
